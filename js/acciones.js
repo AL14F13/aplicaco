75d43aa50fd7d4a3bcb643aef8e002273153e8e5
AL14F13 <BotkkaBela24@gmail.com>
2015-04-21 14:19:14 -0500

    uno
    Archivos nuevos 1 (imc)

---
+++ js/acciones.js
@@ -0,0 +1,30 @@
+// JavaScript Document
+$(document).ready(function(e) {
+// que esperemos a que el dispositivo este listo
+//document.addEventListener("deviceready",function(){
+	$('#btndatos').on('click',function(){
+		//alert('hola');
+		$('body').pagecontainer("change","#datos",
+		{transition:"flip"});
+	}); //click btndatos
+//cuando el dispositivo esta listo para usarce
+//}); 
+});
+// JavaScript Document
+$(document).ready(function(e) {
+// que esperemos a que el dispositivo este listo
+//document.addEventListener("deviceready",function(){
+	$('#btnresultado').on('click',function(){
+		//alert('hola');
+		$('body').pagecontainer("change","#resultado",{transition:"flip"});
+		var imc;
+		var peso=$('#txtpeso').val();
+		var talla=$('#txttalla').val();
+		imc=(peso/(talla * talla));
+		$('#imc').text('Nombre '+$('#txtnombre').val()+ ' imc= '+imc);
+		alert(peso);
+		alert(talla);
+	}); //click btndatos
+//cuando el dispositivo esta listo para usarce
+//}); 
+});
